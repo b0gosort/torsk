d5e68a11464d10c0ab5dc46bdf2c6cca5a5006af
b0gosort <sverige.cooper@gmail.com>
2019-05-10 12:29:07 -0700

    created file for roller command

---
+++ commands/roller.js
@@ -0,0 +1,7 @@
+module.exports = {
+	name: "roller",
+	description: "gir ei rolle eller flere roller til brukeren",
+	execute(config, message, args) {
+		message.channel.send("Denne kommandoen funker ikke ennå.");
+	}
+};
