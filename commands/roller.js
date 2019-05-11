6dea76f7450cd8e170f291da99f2ec5aaed4e8ae
Cooper <b0gosort@users.noreply.github.com>
2019-05-11 01:20:00 -0700

    added some roller command functionality

--- commands/roller.js
+++ commands/roller.js
@@ -2,6 +2,45 @@ module.exports = {
 	name: "roller",
 	description: "gir ei rolle eller flere roller til brukeren",
 	execute(config, message, args) {
-		message.channel.send("Denne kommandoen funker ikke ennå.");
+		var changes = [];
+
+		args.forEach(query => {
+			selection = query.toLowerCase();
+
+			config.roles.selectable.forEach(category => {
+				// Case insensitive check for requested role in category
+				queryCompare = [];
+				category.forEach(rolename => queryCompare.push(rolename.toLowerCase()));
+
+				if (queryCompare.includes(selection)) {
+					// Remove other roles in category from user
+					message.member.roles.forEach(role => {
+						if (category.includes(role.name)) message.member.removeRole(role).then(() => {
+							changes.push(`Rolla ${role.name} ble fjerna.`);
+						}).catch(error => {
+							console.error(error);
+							changes.push(`Rolla ${role.name} kunne ikke fjernes.`);
+						});
+					});
+
+					// Add requested role to user
+					properName = category[queryCompare.indexOf(selection)];
+					message.member.addRole(message.guild.roles.find(role => role.name === properName)).then(() => {
+						changes.push(`Rolla ${properName} ble gitt.`);
+					}).catch(error => {
+						console.error(error);
+						changes.push(`Rolla ${properName} kunne ikke gis.`);
+					});
+				}
+			});
+		});
+
+		changesList = "\n```";
+		changes.forEach(change => changesList += "\n" + change);
+		changesList += "```"
+
+		message.react("✅");
+
+		//message.reply(`følgende endringer har blitt utførte:${changes}`);
 	}
 };
