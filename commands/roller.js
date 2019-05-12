6f440dbe6929c75ba7e0f516f6b9447bb35af2f3
Cooper <b0gosort@users.noreply.github.com>
2019-05-12 13:39:26 -0700

    reworked roller for error feedback

--- commands/roller.js
+++ commands/roller.js
@@ -1,45 +1,66 @@
 module.exports = {
 	name: "roller",
-	description: "gir ei rolle eller flere roller til brukeren",
+	description: "gir éi eller flere roller til brukeren",
 	execute(config, message, args) {
-		//var changes = [];
+		var success = [true];
 
-		args.forEach(query => {
-			selection = query.toLowerCase();
+		var argsLower = [];
+		args.forEach(item => argsLower.push(item.toLowerCase()));
 
+		argsLower.forEach(arg => {
+			// Did user request role not in selectable roles
+			if (!config.roles.selectable.some(category => {
+				let catLower = [];
+				category.forEach(catItem => catLower.push(catItem.toLowerCase()));
+				return catLower.includes(arg);
+			})) {
+				success[0] = false;
+				success.push(`Rolla ${args[args.indexOf(arg)]} kan ikke skaffes med denne kommandoen.`);
+				return;
+			}
+
+			// Go through each category
 			config.roles.selectable.forEach(category => {
-				// Case insensitive check for requested role in category
-				queryCompare = [];
-				category.forEach(rolename => queryCompare.push(rolename.toLowerCase()));
+				let catLower = [];
+				category.forEach(catItem => catLower.push(catItem.toLowerCase()));
 
-				if (queryCompare.includes(selection)) {
-					// Remove other roles in category from user
+				if (catLower.includes(arg)) {
+					// Did user request multiple roles from category
+					otherArgs = argsLower.slice(0, argsLower.indexOf(arg));
+					argsLower.slice(argsLower.indexOf(arg) + 1, argsLower.length).forEach(otherArg => otherArgs.push(otherArg));
+					if (catLower.some(role => otherArgs.includes(role))) {
+						success[0] = false;
+						success.push("Det er ikke mulig å ha mer enn éi rolle i samme kategori.");
+						return;
+					}
+
+					// Remove existing roles in category
 					message.member.roles.forEach(role => {
-						if (category.includes(role.name)) message.member.removeRole(role).then(() => {
-							//changes.push(`Rolla ${role.name} ble fjerna.`);
-						}).catch(error => {
-							console.error(error);
-							//changes.push(`Rolla ${role.name} kunne ikke fjernes.`);
-						});
+						if (category.includes(role.name)) message.member.removeRole(role).catch(error => console.error(error));
 					});
 
-					// Add requested role to user
-					properName = category[queryCompare.indexOf(selection)];
-					message.member.addRole(message.guild.roles.find(role => role.name === properName)).then(() => {
-						//changes.push(`Rolla ${properName} ble gitt.`);
-					}).catch(error => {
-						console.error(error);
-						//changes.push(`Rolla ${properName} kunne ikke gis.`);
-					});
+					// Give role to user
+					roleGiven = message.guild.roles.find(role => role.name === category[catLower.indexOf(arg)]);
+					message.member.addRole(roleGiven).catch(error => console.error(error));
 				}
 			});
 		});
 
-		message.react("✅");
+		if (success[0] === true) {
+			message.react("✅");
+		} else {
+			message.react("❎");
 
-		//changesList = "\n```";
-		//changes.forEach(change => changesList += "\n" + change);
-		//changesList += "```"
-		//message.reply(`følgende endringer har blitt utførte:${changes}`);
+			// Compiling error message
+			response = "det oppstod følgende feil med førespørselen:\n```";
+			errors = [];
+			success.slice(1).forEach(error => {
+				if (!errors.includes(error)) errors.push(error)
+			});
+			errors.forEach(error => response += "\n" + error);
+			response += "```";
+
+			message.reply(response);
+		}
 	}
 };
